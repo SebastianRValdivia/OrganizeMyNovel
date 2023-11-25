b39f7e7339d994327abdbf92d448c5f76abb1290
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-11-25 19:30:26 -0300

    basic main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    # Check if arguments are provided
+    if len(sys.argv) == 1: # If none
+        print("no args") # To be replaced with the ui
+    else: # If there are args
+        for arg in sys.argv[1:]:
+
+
+
