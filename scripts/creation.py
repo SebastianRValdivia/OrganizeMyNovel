5b9ed1f6e7ecafc6d165f462877399a9fc6e5978
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2023-11-25 21:20:19 -0300

    init git in hidden func

--- scripts/creation.py
+++ scripts/creation.py
@@ -1,15 +1,20 @@
 import os
 import subprocess
 
+def _init_git():
+    # Run 'git init' command
+    subprocess.run(["git", "init"])
+    print("Git repository initialized.")
+
+
 def init(novel_name):
     try:
         os.makedirs(novel_name)
         print('Novel folder created successfully.')
         os.chdir(novel_name)
 
-        # Run 'git init' command
-        subprocess.run(["git", "init"])
-        print(f"Git repository initialized.")
+        _init_git()
+
 
     except OSError as e:
         print(f'Error: {e}')
